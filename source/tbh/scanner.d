c99ea30576c7abef3302b0423c66213a0594f58f
kurokoji <shu.kakihana@gmail.com>
2018-01-01 03:26:49 +0900

    [add] add scanner.d

---
+++ source/tbh/scanner.d
@@ -0,0 +1,47 @@
+module tbh.scanner;
+
+
+class Scanner {
+  import std.stdio : File;
+  import std.conv : to;
+  import std.array : split;
+
+  private File file;
+  private dchar[][] str;
+  private uint idx;
+
+  this(File file = stdin) {
+    this.file = file;
+    this.idx = 0;
+  }
+
+  private dchar[] next() {
+    if (idx < str.length) {
+      return str[idx++];
+    }
+
+    dchar[] s;
+    while (s.length == 0) {
+      s = file.readln.chomp.to!(dchar[]);
+    }
+
+    str = s.split;
+    idx = 0;
+
+    return str[idx++];
+  }
+
+  T next(T)() {
+    return next.to!(T);
+  }
+
+  T[] next(T : T[])(uint len) {
+    T[] ret = new T[len];
+
+    foreach (ref c; ret) {
+      c = next!(T);
+    }
+
+    return ret;
+  }
+}
