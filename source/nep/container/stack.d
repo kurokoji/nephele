eb40dbe010f79d86d3f64782490f81d4bdb9221e
kurokoji <shu.kakihana@gmail.com>
2018-04-20 22:05:42 +0900

    [update] add Payload struct

--- source/nep/container/stack.d
+++ source/nep/container/stack.d
@@ -3,4 +3,11 @@ module nep.container.stack;
 
 struct Stack(T)
 {
+    private struct Payload(T, size_t _minCapacity = 5) if (_minCapacity >= 0)
+    {
+        private T* _data;
+        private size_t _capacity, _length;
+
+    }
+
 }
