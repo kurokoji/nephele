a5c2ef6106da6e1252392ef49e6d7dd2b7844955
kurokoji <shu.kakihana@gmail.com>
2018-04-20 22:08:33 +0900

    [update] add basic member functions
    emplace()
    pop()
    free()
    reserve()
    crear()

--- source/nep/container/stack.d
+++ source/nep/container/stack.d
@@ -16,6 +16,56 @@ struct Stack(T)
                 GC.free(_data);
             }
         }
+
+        void reserve(size_t newCapacity)
+        {
+            import core.memory : GC;
+            import core.stdc.string : memcpy;
+
+            if (_capacity >= newCapacity) return;
+
+            T* newData = cast(T*)GC.malloc(T.sizeof * newCapacity);
+            if (_length != 0) memcpy(newData, _data, _length * T.sizeof);
+            _capacity = newCapacity;
+
+            T* oldData = _data;
+            _data = newData;
+            GC.free(oldData);
+        }
+
+        void free()
+        {
+            import core.memory : GC;
+            GC.free(_data);
+            _length = 0;
+            _capacity = 0;
+        }
+
+        void crear()
+        {
+            _length = 0;
+        }
+
+        void emplace()(const ref auto T value)
+        {
+            if (_length == _capacity)
+            {
+                if (_capacity < _minCapacity)
+                {
+                    reserve(_minCapacity);
+                }
+                else
+                {
+                    reserve(_capacity * 2);
+                }
+            }
+            _data[_length++] = value;
+        }
+
+        void pop()
+        {
+            --_length;
+        }
     }
 
 }
