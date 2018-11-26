1612782a7332b72752402cac286467cf2e4c2ac8
kurokoji <shu.kakihana@gmail.com>
2018-11-26 21:28:26 +0900

    style: format

--- source/nep/container/deque.d
+++ source/nep/container/deque.d
@@ -1,273 +1,230 @@
 module nep.container.deque;
 
-struct DequePayload(T, size_t pageSize = 8)
-{
-    import std.traits;
-    import core.exception : RangeError;
+struct DequePayload(T, size_t pageSize = 8) {
+  import std.traits;
+  import core.exception : RangeError;
 
-    private
-    {
-        T[][] _data;
-        size_t top, len, cap;
+  private {
+    T[][] _data;
+    size_t top, len, cap;
+  }
+
+  alias opDollar = length;
+  alias opOpAssign(string op : "~") = insertBack;
+
+  @property {
+    size_t empty() const {
+      return len == 0;
     }
 
-    alias opDollar = length;
-    alias opOpAssign(string op : "~") = insertBack;
-
-    @property
-    {
-        size_t empty() const
-        {
-            return len == 0;
-        }
-
-        size_t length() const
-        {
-            return len;
-        }
-
-        ref inout(T) front() inout
-        {
-            return this[0];
-        }
-
-        ref inout(T) back() inout
-        {
-            return this[$ - 1];
-        }
+    size_t length() const {
+      return len;
     }
 
-    ref inout(T) opIndex(size_t idx) inout
-    {
-        if (len <= idx)
-        {
-            throw new RangeError();
-        }
-
-        if (top + idx < cap)
-        {
-            return _data[(top + idx) / pageSize][(top + idx) % pageSize];
-        }
-
-        return _data[(top + idx - cap) / pageSize][(top + idx - cap) % pageSize];
+    ref inout(T) front() inout {
+      return this[0];
     }
 
-    void reserve(size_t newCap)
-    {
-        size_t nextCap = 1;
-        while (nextCap < newCap)
-            nextCap <<= 1;
+    ref inout(T) back() inout {
+      return this[$ - 1];
+    }
+  }
 
-        import std.math : abs;
-
-        if (nextCap <= cap)
-            return;
-
-        size_t newArrSize = nextCap / pageSize - cast(size_t)(nextCap % pageSize == 0) + 1;
-        size_t nowArrSize = cap / pageSize - cast(size_t)(cap % pageSize == 0) + 1;
-        if (newArrSize <= nowArrSize)
-            return;
-
-        foreach (i; 0 .. newArrSize - nowArrSize)
-        {
-            _data ~= new T[pageSize];
-        }
-
-        top = 0;
-        cap = nextCap;
+  ref inout(T) opIndex(size_t idx) inout {
+    if (len <= idx) {
+      throw new RangeError();
     }
 
-    void insertBack(E)(E value) if (isImplicitlyConvertible!(E, T))
-    {
-        import std.algorithm : max;
-
-        if (len == cap)
-        {
-            reserve(max(cap * 2 * pageSize, 4 * pageSize));
-        }
-
-        ++len;
-        this[$ - 1] = value;
+    if (top + idx < cap) {
+      return _data[(top + idx) / pageSize][(top + idx) % pageSize];
     }
 
-    void popBack()
-    {
-        assert(!empty, "nep.container.Deque.removeBack: Deque is empty.");
+    return _data[(top + idx - cap) / pageSize][(top + idx - cap) % pageSize];
+  }
 
-        --len;
+  void reserve(size_t newCap) {
+    size_t nextCap = 1;
+    while (nextCap < newCap)
+      nextCap <<= 1;
+
+    import std.math : abs;
+
+    if (nextCap <= cap)
+      return;
+
+    size_t newArrSize = nextCap / pageSize - cast(size_t)(nextCap % pageSize == 0) + 1;
+    size_t nowArrSize = cap / pageSize - cast(size_t)(cap % pageSize == 0) + 1;
+    if (newArrSize <= nowArrSize)
+      return;
+
+    foreach (i; 0 .. newArrSize - nowArrSize) {
+      _data ~= new T[pageSize];
     }
 
-    void clear()
-    {
-        top = 0;
-        len = 0;
+    top = 0;
+    cap = nextCap;
+  }
+
+  void insertBack(E)(E value) if (isImplicitlyConvertible!(E, T)) {
+    import std.algorithm : max;
+
+    if (len == cap) {
+      reserve(max(cap * 2 * pageSize, 4 * pageSize));
     }
 
-    void insertFront(E)(E value) if (isImplicitlyConvertible!(E, T))
-    {
-        import std.algorithm : max;
+    ++len;
+    this[$ - 1] = value;
+  }
 
-        if (len == cap)
-        {
-            reserve(max(cap * 2 * pageSize, 4 * pageSize));
-        }
+  void popBack() {
+    assert(!empty, "nep.container.Deque.removeBack: Deque is empty.");
 
-        if (top == 0)
-        {
-            top += cap;
-        }
+    --len;
+  }
 
-        --top;
-        ++len;
-        this[0] = value;
+  void clear() {
+    top = 0;
+    len = 0;
+  }
+
+  void insertFront(E)(E value) if (isImplicitlyConvertible!(E, T)) {
+    import std.algorithm : max;
+
+    if (len == cap) {
+      reserve(max(cap * 2 * pageSize, 4 * pageSize));
     }
 
-    void popFront()
-    {
-        assert(!empty, "nep.container.Deque.removeFront: Deque is empty.");
-
-        ++top;
-        --len;
-        if (top == cap)
-        {
-            top = 0;
-        }
+    if (top == 0) {
+      top += cap;
     }
 
-    string toString() const
-    {
-        import std.format : format;
+    --top;
+    ++len;
+    this[0] = value;
+  }
 
-        string ret = "[ ";
-        foreach (i; 0 .. len)
-        {
-            ret ~= format("%s%s", this[i], i == len - 1 ? " ]" : ", ");
-        }
+  void popFront() {
+    assert(!empty, "nep.container.Deque.removeFront: Deque is empty.");
 
-        return ret;
+    ++top;
+    --len;
+    if (top == cap) {
+      top = 0;
     }
+  }
+
+  string toString() const {
+    import std.format : format;
+
+    string ret = "[ ";
+    foreach (i; 0 .. len) {
+      ret ~= format("%s%s", this[i], i == len - 1 ? " ]" : ", ");
+    }
+
+    return ret;
+  }
 }
 
-struct Deque(T, bool mayNull = true)
-{
-    import std.traits : isImplicitlyConvertible;
-    import std.range : ElementType, isInputRange;
-    import std.exception;
+struct Deque(T, bool mayNull = true) {
+  import std.traits : isImplicitlyConvertible;
+  import std.range : ElementType, isInputRange;
+  import std.exception;
 
-    alias Payload = DequePayload!T;
-    alias opDollar = length;
-    alias opOpAssign(string op : "~") = insertBack;
+  alias Payload = DequePayload!T;
+  alias opDollar = length;
+  alias opOpAssign(string op : "~") = insertBack;
 
-    private
-    {
-        Payload* p;
+  private {
+    Payload* p;
+  }
+
+  this(A)(A[] args...) if (isImplicitlyConvertible!(A, T)) {
+    p = new Payload();
+
+    foreach (e; args) {
+      p ~= e;
+    }
+  }
+
+  @property {
+    bool empty() const {
+      return !(!mayNull || p) || p.empty;
     }
 
-    this(A)(A[] args...) if (isImplicitlyConvertible!(A, T))
-    {
-        p = new Payload();
-
-        foreach (e; args)
-        {
-            p ~= e;
-        }
+    size_t length() const {
+      return !mayNull ? p.length : 0;
     }
 
-    @property
-    {
-        bool empty() const
-        {
-            return !(!mayNull || p) || p.empty;
-        }
-
-        size_t length() const
-        {
-            return !mayNull ? p.length : 0;
-        }
-
-        ref inout(T) front() inout
-        {
-            return p.front;
-        }
-
-        ref inout(T) back() inout
-        {
-            return p.back;
-        }
+    ref inout(T) front() inout {
+      return p.front;
     }
 
-    ref inout(T) opIndex(size_t idx) inout
-    {
-        assert (!empty, "nep.container.deque.opIndex: Deque is empty.");
+    ref inout(T) back() inout {
+      return p.back;
+    }
+  }
 
-        return (*p)[idx];
+  ref inout(T) opIndex(size_t idx) inout {
+    assert(!empty, "nep.container.deque.opIndex: Deque is empty.");
+
+    return (*p)[idx];
+  }
+
+  void insertBack(T value) {
+    if (mayNull && !p) {
+      p = new Payload();
     }
 
-    void insertBack(T value)
-    {
-        if (mayNull && !p)
-        {
-            p = new Payload();
-        }
+    p.insertBack(value);
+  }
 
-        p.insertBack(value);
+  void popBack() {
+    assert(!mayNull || p, "nep.container.deque.popFront: Deque is empty.");
+
+    p.popBack();
+  }
+
+  void insertFront(T value) {
+    if (mayNull && !p) {
+      p = new Payload();
     }
 
-    void popBack()
-    {
-        assert(!mayNull || p, "nep.container.deque.popFront: Deque is empty.");
+    p.insertFront(value);
+  }
 
-        p.popBack();
+  void popFront() {
+    assert(!mayNull || p, "nep.container.deque.popFront: Deque is empty.");
+
+    p.popFront();
+  }
+
+  void clear() {
+    if (p) {
+      p.clear();
     }
+  }
 
-    void insertFront(T value)
-    {
-        if (mayNull && !p)
-        {
-            p = new Payload();
-        }
-
-        p.insertFront(value);
-    }
-
-    void popFront()
-    {
-        assert(!mayNull || p, "nep.container.deque.popFront: Deque is empty.");
-
-        p.popFront();
-    }
-
-    void clear()
-    {
-        if (p)
-        {
-            p.clear();
-        }
-    }
-
-    string toString() const
-    {
-        return !empty ? p.toString() : "[  ]";
-    }
+  string toString() const {
+    return !empty ? p.toString() : "[  ]";
+  }
 }
 
-unittest
-{
-    auto dq = Deque!int();
+unittest {
+  auto dq = Deque!int();
 
-    dq.insertBack(1);
-    dq.insertFront(2);
-    dq.insertBack(3);
-    dq.insertFront(4);
-    dq.insertBack(5);
+  dq.insertBack(1);
+  dq.insertFront(2);
+  dq.insertBack(3);
+  dq.insertFront(4);
+  dq.insertBack(5);
 
-    assert(dq.toString() == "[ 4, 2, 1, 3, 5 ]");
-    dq.popBack();
-    assert(dq.toString() == "[ 4, 2, 1, 3 ]");
-    dq.insertBack(6);
-    assert(dq.toString() == "[ 4, 2, 1, 3, 6 ]");
-    dq.popFront();
-    assert(dq.toString() == "[ 2, 1, 3, 6 ]");
-    dq.insertFront(7);
-    assert(dq.toString() == "[ 7, 2, 1, 3, 6 ]");
+  assert(dq.toString() == "[ 4, 2, 1, 3, 5 ]");
+  dq.popBack();
+  assert(dq.toString() == "[ 4, 2, 1, 3 ]");
+  dq.insertBack(6);
+  assert(dq.toString() == "[ 4, 2, 1, 3, 6 ]");
+  dq.popFront();
+  assert(dq.toString() == "[ 2, 1, 3, 6 ]");
+  dq.insertFront(7);
+  assert(dq.toString() == "[ 7, 2, 1, 3, 6 ]");
 }
--- source/nep/container/stack.d
+++ source/nep/container/stack.d
@@ -1,122 +1,102 @@
 module nep.container.stack;
 
-struct Stack(T, size_t minCapacity = 5) if (minCapacity >= 0)
-{
-    import std.traits;
-    import std.range.primitives;
+struct Stack(T, size_t minCapacity = 5) if (minCapacity >= 0) {
+  import std.traits;
+  import std.range.primitives;
 
-    private
-    {
-        T* _data;
-        size_t _capacity, _length;
+  private {
+    T* _data;
+    size_t _capacity, _length;
+  }
+
+  alias opOpAssign(string op = "~") = insertBack;
+
+  ~this() {
+    import core.memory : GC;
+
+    if (_data != null) {
+      GC.free(_data);
+    }
+  }
+
+  @property {
+    size_t length() const {
+      return _length;
     }
 
-    alias opOpAssign(string op = "~") = insertBack;
-
-    ~this()
-    {
-        import core.memory : GC;
-
-        if (_data != null)
-        {
-            GC.free(_data);
-        }
+    size_t capacity() const {
+      return _capacity;
     }
 
-    @property
-    {
-        size_t length() const
-        {
-            return _length;
-        }
-
-        size_t capacity() const
-        {
-            return _capacity;
-        }
-
-        bool empty() const
-        {
-            return _length == 0;
-        }
-
-        ref inout(T) back() inout
-        {
-            assert(_length != 0, "nep.container.Stack.top: Stack is empty");
-            return _data[_length - 1];
-        }
-
+    bool empty() const {
+      return _length == 0;
     }
 
-    void reserve(size_t newCapacity)
-    {
-        import core.memory : GC;
-        import core.stdc.string : memcpy;
-
-        if (_capacity >= newCapacity)
-            return;
-
-        T* newData = cast(T*) GC.malloc(T.sizeof * newCapacity);
-        if (_length != 0)
-            memcpy(newData, _data, _length * T.sizeof);
-        _capacity = newCapacity;
-
-        T* oldData = _data;
-        _data = newData;
-        GC.free(oldData);
+    ref inout(T) back() inout {
+      assert(_length != 0, "nep.container.Stack.top: Stack is empty");
+      return _data[_length - 1];
     }
 
-    void free()
-    {
-        import core.memory : GC;
+  }
 
-        GC.free(_data);
-        _length = 0;
-        _capacity = 0;
-    }
+  void reserve(size_t newCapacity) {
+    import core.memory : GC;
+    import core.stdc.string : memcpy;
 
-    void crear()
-    {
-        _length = 0;
-    }
+    if (_capacity >= newCapacity)
+      return;
 
-    void insertBack(E)(E value) if (isImplicitlyConvertible!(E, T))
-    {
-        if (_length == _capacity)
-        {
-            if (_capacity < minCapacity)
-            {
-                reserve(minCapacity);
-            }
-            else
-            {
-                reserve(1 + _capacity * 3 / 2);
-            }
-        }
-        _data[_length++] = value;
-    }
+    T* newData = cast(T*)GC.malloc(T.sizeof * newCapacity);
+    if (_length != 0)
+      memcpy(newData, _data, _length * T.sizeof);
+    _capacity = newCapacity;
 
-    void popBack()
-    {
-        assert(_length != 0, "nep.container.Stack.pop: Stack is empty");
-        --_length;
+    T* oldData = _data;
+    _data = newData;
+    GC.free(oldData);
+  }
+
+  void free() {
+    import core.memory : GC;
+
+    GC.free(_data);
+    _length = 0;
+    _capacity = 0;
+  }
+
+  void crear() {
+    _length = 0;
+  }
+
+  void insertBack(E)(E value) if (isImplicitlyConvertible!(E, T)) {
+    if (_length == _capacity) {
+      if (_capacity < minCapacity) {
+        reserve(minCapacity);
+      } else {
+        reserve(1 + _capacity * 3 / 2);
+      }
     }
+    _data[_length++] = value;
+  }
+
+  void popBack() {
+    assert(_length != 0, "nep.container.Stack.pop: Stack is empty");
+    --_length;
+  }
 }
 
-@system unittest
-{
-    import std.stdio : writeln;
+@system unittest {
+  import std.stdio : writeln;
 
-    auto st = Stack!int();
-    st ~= 1;
-    st ~= 10;
-    assert(st.back == 10);
-    st.popBack();
-    assert(st.back == 1);
+  auto st = Stack!int();
+  st ~= 1;
+  st ~= 10;
+  assert(st.back == 10);
+  st.popBack();
+  assert(st.back == 1);
 }
 
-@system unittest
-{
-    auto st = Stack!int();
-    assert(st.empty);
+@system unittest {
+  auto st = Stack!int();
+  assert(st.empty);
 }
